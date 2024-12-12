f7c10ee1d346d20dd690c10aca137bc30d11c875
7RosenRot <157651503+7RosenRot@users.noreply.github.com>
2024-12-12 20:53:00 +0300

    Delete Laboratory Work 3 java directory

--- Laboratory Work 3 java/src/main/java/org/example/interfaceCommand.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-interface interfaceCommand {
-    void commandExecute(interfaceCPU CPU);
-    public String Instruction_get();
-    public String[] partInstruction_get();
-}
--- Laboratory Work 3 java/src/main/java/org/example/task.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface task {
-    int task(int value);
-}
